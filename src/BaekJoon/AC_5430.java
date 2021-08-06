04db3ad1fd25aed60bcf86a9b6627c29375de5ad
parkjoohan <joohanpark1513@gmail.com>
2021-08-07 01:45:59 +0900

    UPDATE(21.08.07)

---
+++ src/BaekJoon/AC_5430.java
@@ -0,0 +1,33 @@
+package BaekJoon;
+
+import java.util.*;
+
+public class AC_5430 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		StringTokenizer st = new StringTokenizer(null)
+		int T =sc.nextInt();
+	
+		for(int i=1; i<=T; i++) {
+			// 함수
+			String func = sc.next();
+			// 배열의 수 개수
+			int num = sc.nextInt();
+			// 배열 수 입력
+			int[] arr = new int[num];
+			for(int j=0; j<num; j++)
+				arr[j] = sc.nextInt();
+		}
+		
+		while(true) {
+			
+		}
+
+	}
+	
+	public static void RDD() {
+		
+	}
+
+}
