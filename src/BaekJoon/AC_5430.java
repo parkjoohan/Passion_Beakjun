b010f74b1a0f1c2354fa23cb14880da911c2a364
parkjoohan <joohanpark1513@gmail.com>
2021-08-07 21:48:55 +0900

    UPDATE(21.08.07)

--- src/BaekJoon/AC_5430.java
+++ src/BaekJoon/AC_5430.java
@@ -1,33 +1,26 @@
 package BaekJoon;
 
+import java.io.*;
 import java.util.*;
 
 public class AC_5430 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		StringTokenizer st = new StringTokenizer(null)
-		int T =sc.nextInt();
+		StringBuilder sb = new StringBuilder();
+		int T = sc.nextInt();
+		Deque<Integer> d = new ArrayDeque<Integer>();
 	
 		for(int i=1; i<=T; i++) {
 			// 함수
 			String func = sc.next();
 			// 배열의 수 개수
 			int num = sc.nextInt();
+			StringTokenizer st = new StringTokenizer(sc.next(), "[],");
 			// 배열 수 입력
-			int[] arr = new int[num];
-			for(int j=0; j<num; j++)
-				arr[j] = sc.nextInt();
+			for(int j=0; j<num; j++) {
+				d.add(Integer.parseInt(st.nextToken()));
+			}
 		}
-		
-		while(true) {
-			
-		}
-
 	}
-	
-	public static void RDD() {
-		
-	}
-
 }
