58813373854337760a11bc0726fecb4de8fede00
parkjoohan <joohanpark1513@gmail.com>
2021-08-07 23:52:02 +0900

    UPDATE(21.08.07)

---
+++ src/BaekJoon/치킨배달_15686.java
@@ -0,0 +1,23 @@
+package BaekJoon;
+
+import java.util.*;
+
+public class 치킨배달_15686 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int M = sc.nextInt();
+		int[][] arr = new int[N][N];
+		
+		for(int i=0; i<N; i++) {
+			for(int j=0; j<N; j++) {
+				arr[i][j] = sc.nextInt();
+			}
+		}
+		
+		
+
+	}
+
+}
