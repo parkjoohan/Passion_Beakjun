cb8558e0c9f59e352bf72c01243b8dd1e38b1262
parkjoohan <joohanpark1513@gmail.com>
2021-08-10 17:07:42 +0900

    UPDATE(21.08.10)

---
+++ src/BaekJoon/트리순회_1991.java
@@ -0,0 +1,17 @@
+package BaekJoon;
+
+import java.util.*;
+
+public class 트리순회_1991 {
+
+	public static void main(String[] args) {
+		LinkedList<String> list = new LinkedList<String>();
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		
+		for(int i=1; i<=T; i++) {
+			String N = sc.next().replace(".","");
+			
+		}
+	}
+}
