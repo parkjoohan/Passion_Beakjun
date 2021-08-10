a348eb59787b804c66fa2877d1051ccd1e328c77
parkjoohan <joohanpark1513@gmail.com>
2021-08-10 22:52:13 +0900

    UPDATE(21.08.10)

---
+++ src/BaekJoon/이진트리검색_5639.java
@@ -0,0 +1,50 @@
+package BaekJoon;
+
+import java.io.*;
+ 
+public class 이진트리검색_5639 {
+ 
+    static class Tree{
+        Node root;
+ 
+        void add(Node node, int data) {
+            if (node == null) {
+                root = new Node(data);
+            } else {
+                if (data > node.data) {
+                    if(node.right != null) add(node.right, data);
+                    else node.right = new Node(data);
+                } else if(data < node.data) {
+                    if(node.left != null) add(node.left, data);
+                    else node.left = new Node(data);
+                }
+            }
+        }
+ 
+        void postOrder(Node node){
+            if (node.left != null) postOrder(node.left);
+            if (node.right != null) postOrder(node.right);
+            System.out.println(node.data);
+        }
+    }
+ 
+    static class Node{
+        int data;
+        Node left,right;
+ 
+        Node(int data) {
+            this.data = data;
+        }
+    }
+ 
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Tree tree = new Tree();
+        String value = "";
+        while ((value = br.readLine()) != null) {
+            int data = Integer.parseInt(value);
+            tree.add(tree.root, data);
+        }
+        tree.postOrder(tree.root);
+    }
+}
