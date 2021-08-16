29c4a74d85b1ea9ad48c7764300efd69acd86f0c
parkjoohan <joohanpark1513@gmail.com>
2021-08-16 15:46:26 +0900

    UPDATE(21.08.16)

---
+++ src/BaekJoon/조합_2407.java
@@ -0,0 +1,34 @@
+package BaekJoon;
+
+import java.util.*;
+import java.io.*;
+
+public class 조합_2407 {
+
+	static int N, M;
+	static int totalCnt;
+	static int[] numbers;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		numbers = new int[M];
+		totalCnt=0;
+				
+		Combination(0,0);
+		System.out.println(totalCnt);
+	}
+	
+	private static void Combination(int cnt, int start) {
+		if(cnt == M) {
+			return;
+		}
+		
+		for(int i=start; i<=N; i++) {
+			numbers[cnt] = i;
+			Combination(cnt+1, i+1);
+			totalCnt++;
+		}
+	}
+}
