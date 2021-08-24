3c93b9f77a41ff1a042ff75ef99bf5706b192e33
parkjoohan <joohanpark1513@gmail.com>
2021-08-25 01:49:13 +0900

    UPDATE(21.08.25)

--- src/BaekJoon/P_10163_색종이.java
+++ src/BaekJoon/P_10163_색종이.java
@@ -1,36 +1,41 @@
 package BaekJoon;
 
+import java.io.*;
 import java.util.*;
 
 public class P_10163_색종이 {
-
+	
 	static int N;
 	static int[][] map;
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		N = sc.nextInt();
-		map = new int[101][101];
-
-		for(int tc=1; tc<=N; tc++) {
-			int x = sc.nextInt();
-			int y = sc.nextInt();
-			int a = sc.nextInt();
-			int b = sc.nextInt();
-	
-			for(int i=x; i<x+a; i++) {
-				for(int j=y; j<y+b; j++) {
-					map[i][j] = tc;
-				}
-			}
-		}
-		for(int tcc=1; tcc<=N; tcc++) {
-			int cnt=0;
-			for(int i=0; i<101; i++) {
-				for(int j=0; j<101; j++) {
-					if(map[i][j] == tcc)	cnt++;
-				}
-			}
-			System.out.println(cnt);
-		}
-	}
+	private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String args[]) throws IOException {
+       
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        map = new int[101][101];
+        
+        for (int i = 1; i <= N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            for (int j = x; j < x + a; j++) {
+                for (int k = y; k < y + b; k++) {
+                    map[j][k] = i;
+                }
+            }
+        }
+        for (int z = 1; z <= N; z++) {
+            int cnt = 0;
+            for (int i = 0; i < 101; i++) {
+                for (int j = 0; j < 101; j++) {
+                    if (map[i][j] == z) 
+                    	cnt++;
+                }
+            }
+            bw.write(cnt + "\n");
+        }
+        bw.flush();
+    }
 }
