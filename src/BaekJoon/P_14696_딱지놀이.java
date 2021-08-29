a4e83a5b3df1adfe2aeb95942a980d36c51204a7
parkjoohan <joohanpark1513@gmail.com>
2021-08-29 14:43:19 +0900

    UPDATE(21.08.29)

--- src/BaekJoon/P_14696_딱지놀이.java
+++ src/BaekJoon/P_14696_딱지놀이.java
@@ -4,23 +4,38 @@ import java.util.*;
 
 public class P_14696_딱지놀이 {
 
-	public static void main(String[] args) {
+	static int N; // 총 라운드 수
+	public static void main(String[] args) {	
 		Scanner sc = new Scanner(System.in);
-		int N =sc.nextInt();
-		int[] A = new int[100];
-		int[] B = new int[100];
-		
-		for (int i = 0; i < N; i++) {
-			int T = sc.nextInt();
-			for (int j = 0; j < T; j++) {
-				A[j] = sc.nextInt();
+		N = sc.nextInt();	
+		for ( int i=0; i<N; i++) {
+			int A = sc.nextInt();
+			int[] cardA = new int[5];
+			for(int j=0; j<A; j++) {	// A 어린이 카드 개수 저장
+				cardA[sc.nextInt()]++;
 			}
-			int R = sc.nextInt();
-			for (int j = 0; j < R; j++) {
-				B[j] = sc.nextInt();
+			
+			int B = sc.nextInt();
+			int[] cardB = new int[5];
+			for(int j=0;j<B;j++) {	// B 어린이 카드 개수 저장
+				cardB[sc.nextInt()] ++;
 			}
+			
+			boolean flag = false;
+			for(int j=4;j>0;j--) {
+				if(cardA[j]>cardB[j]) {		// 4의 개수 체크
+					System.out.println("A");
+					break;
+				}
+				else if(cardA[j]<cardB[j]){
+					System.out.println("B");
+					break;
+				}
+				if(j==1 && cardA[j]==cardB[j]) 
+					flag = true;
+			}
+			if(flag) 	// 만약 비겼으면 D 출력
+				System.out.println("D");
 		}
-
 	}
-
 }
