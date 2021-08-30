b586885acd8e02b685d448a011cc3400e9f5803b
parkjoohan <joohanpark1513@gmail.com>
2021-08-30 09:58:55 +0900

    UPDATE(21.08.30)

--- src/BaekJoon/P_1193_분수찾기.java
+++ src/BaekJoon/P_1193_분수찾기.java
@@ -5,14 +5,16 @@ import java.util.*;
 public class P_1193_분수찾기 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int T = sc.nextInt();
-		int cnt=0;
-		int N=0;
+		int num = sc.nextInt();
+		int i;
 		
-		while(true) {
-			N++;
-			cnt++;
-			if(cnt>=T)
+		for (i = 0; i < num; i++) {
+			num = num-i;
 		}
+		
+		if(i%2==1)
+			System.out.printf("%d/%d", i-(num-1), num);
+		else
+			System.out.printf("%d/%d", num, i-(num-1));
 	}
 }
