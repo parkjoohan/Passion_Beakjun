d0b6c8a318a828a076ffb5e6535139fa3623097e
박주한 <alex0317@naver.com>
2021-09-07 23:58:15 +0900

    UPDATE(21.09.07)

--- src/BaekJoon/P_2740_행렬곱셈.java
+++ src/BaekJoon/P_2740_행렬곱셈.java
@@ -1,10 +1,40 @@
 package BaekJoon;
 
+import java.util.*;
+
 public class P_2740_행렬곱셈 {
 
+	static int N,M,K;
+	static int[][] arr1;
+	static int[][] arr2;
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		M = sc.nextInt();
+		int[][] arr1 = new int[N][M];
+		for(int i=0; i<N; i++)
+			for(int j=0; j<M; j++)
+				arr1[i][j]=sc.nextInt();
+		
+		M = sc.nextInt();
+		K = sc.nextInt();
+		int[][] arr2 = new int[M][K];
+		for(int i=0; i<M; i++)
+			for(int j=0; j<K; j++)
+				arr2[i][j]=sc.nextInt();
+		
+		int[][] result = new int[N][K];
+		for(int i=0; i<N; i++)
+			for(int j=0; j<K; j++)
+				for(int r=0; r<M; r++)
+					result[i][j] += arr1[i][r] * arr2[r][j];
+		
+		for(int i=0; i<N; i++) {
+			for(int j=0; j<K; j++) {
+				System.out.print(result[i][j]);
+				System.out.print(" ");
+			}
+			System.out.println();
+		}
 	}
-
 }
