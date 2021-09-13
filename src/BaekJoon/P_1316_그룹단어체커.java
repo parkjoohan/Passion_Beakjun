6d60642664086fb777f037e05868e6015cf0883c
박주한 <alex0317@naver.com>
2021-09-13 23:27:37 +0900

    UPDATE(21.09.13)

---
+++ src/BaekJoon/P_1316_그룹단어체커.java
@@ -0,0 +1,21 @@
+package BaekJoon;
+
+import java.util.*;
+
+public class P_1316_그룹단어체커 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		
+		for (int i = 0; i < N; i++) {
+			String str = sc.next();
+			char[] arr = new char[str.length()];
+			
+			for (int j = 0; j < str.length(); j++) {
+				arr[j] = 
+			}
+		}
+	}
+
+}
