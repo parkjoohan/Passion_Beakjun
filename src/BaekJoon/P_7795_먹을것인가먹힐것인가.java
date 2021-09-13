b4e348eda09f600845801907c976b6a9bff4329d
박주한 <alex0317@naver.com>
2021-09-14 01:15:47 +0900

    UPDATE(21.09.14)

--- src/BaekJoon/P_7795_먹을것인가먹힐것인가.java
+++ src/BaekJoon/P_7795_먹을것인가먹힐것인가.java
@@ -1,6 +1,32 @@
 package BaekJoon;
 
-import java.util.*;
+import java.io.*; 
+import java.util.*; 
+
+/*public class P_7795_먹을것인가먹힐것인가 {
+	public static void main(String[] args) throws IOException { 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		StringBuilder sb = new StringBuilder(); 
+		int T = Integer.parseInt(br.readLine()); 
+		while (T-->0){ StringTokenizer st = new StringTokenizer(br.readLine()); 
+		
+		int[] A = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).sorted().toArray(); 
+		int[] B = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).sorted().toArray(); 
+		
+		int count = 0; 
+		
+		for (int i : A) 
+			for (int j : B) { 
+				if (i<=j) 
+					break; 
+				else 
+					count ++; 
+			} 
+			sb.append(count+"\n"); 
+		} 
+		System.out.print(sb); 
+	} 
+}*/
 
 public class P_7795_먹을것인가먹힐것인가 {
 
@@ -23,17 +49,21 @@ public class P_7795_먹을것인가먹힐것인가 {
 				arr2[i] = sc.nextInt();
 			}
 			
-			int cnt = 0;
+			int cnt = 1;
 			
 			// start를 차롇대로 증가시키며 반복
-			for(int i = 0; i <arr1.length; i++) {
-				for (int j = 0; j < arr2.length; j++) {
-					if(arr1[i] > arr2[j]) {
+			for(int i : arr1) {
+				for (int j : arr2) {
+					if(i>j) {
 						cnt++;
+					}
+					else {
+						break;
 					}
 				}
 			}
 			System.out.println(cnt);
 		}
+		sc.close();
 	}
 }
