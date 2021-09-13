3bf1d62dca9d87bcc1e5447e5424960081b06b19
박주한 <alex0317@naver.com>
2021-09-13 23:51:40 +0900

    UPDATE(21.09.13)

--- src/BaekJoon/P_1940_주몽.java
+++ src/BaekJoon/P_1940_주몽.java
@@ -4,9 +4,28 @@ import java.util.*;
 
 public class P_1940_주몽 {
 
+	static int N, M;
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-
+		N = sc.nextInt();
+		M = sc.nextInt();
+		int[] arr = new int[N];
+		
+		for (int i = 0; i < N; i++) {
+			arr[i] = sc.nextInt();
+		}
+	
+		int cnt = 0;
+		
+		// start를 차롇대로 증가시키며 반복
+		for(int start = 0; start <arr.length-1; start++) {
+			for (int end = start+1; end < arr.length; end++) {
+				if(arr[start] + arr[end] == M) {
+					cnt++;
+					break;
+				}
+			}
+		}
+		System.out.println(cnt);
 	}
-
 }
