a3c59f8a789d0ef035f6339965f20bf19d0fb3ed
parkjoohan <joohanpark1513@gmail.com>
2021-09-16 01:33:58 +0900

    UPDATE(21.09.16)

--- src/BaekJoon/P_1260_DFS와BFS.java
+++ src/BaekJoon/P_1260_DFS와BFS.java
@@ -4,55 +4,52 @@ import java.util.*;
 
 public class P_1260_DFS와BFS {
  
-    static int n, m, v;
-    static boolean [] visits;
-    static int [][] map;
-    
+	static int N, M, V;
+	static boolean[] visits;	// 확인 여부
+	static int[][] map;			// 간선 연결상태
     public static void main(String[] args) {
- 
-        Scanner sc = new Scanner(System.in);        
-        n = sc.nextInt(); //4
-        m = sc.nextInt(); //5
-        v = sc.nextInt(); //1
-        visits = new boolean [n+1];
-        map = new int [n+1][n+1];
-        
-        for(int i=1; i<=m; i++) {
-            int a = sc.nextInt(); //1
-            int b = sc.nextInt(); //2
-            map[a][b] = 1;
-            map[b][a] = 1;
-        }        
-        dfs(v); //1   
-        System.out.println();        
-        visits = new boolean [n+1];       
-        bfs(v); //1
-    }
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		M = sc.nextInt();
+		V = sc.nextInt();
+		visits = new boolean[N+1];
+		map = new int[N+1][N+1];
+		
+		for (int i = 1; i <= M; i++) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			map[a][b]=1;
+			map[b][a]=1;
+		}
+		dfs(V);
+		System.out.println();
+		visits = new boolean[N+1];
+		bfs(V);
+	}
     
     private static void dfs(int start) {
-        visits[start] = true; //visits[1] = true
-        System.out.print(start + " ");
-        for(int i=1; i<=n; i++) {
-            if (map[i][start] == 1 && !visits[i]) {
-                dfs(i);
-            }
-        }
+    	visits[start] = true;
+    	System.out.print(start+" ");
+    	for (int i = 1; i <= N; i++) {
+			if(map[i][start] == 1 && !visits[i])
+				dfs(i);
+		}
     }
     
     private static void bfs(int start) {
-        Queue<Integer> q = new LinkedList<Integer>();
-        q.offer(start); // 1 
-        while(!q.isEmpty()) {
-            int out = q.poll();
-            visits[out] = true;
-            System.out.print(out + " ");
-            for(int i=1; i<=n; i++) {
-                if (map[i][out] == 1 && !visits[i] && i!=out) { // 2 3 4 
-                    q.offer(i);
-                    visits[i] = true;
-//                    System.out.println(Arrays.toString(q.toArray()));
-                }
-            }       
-        }
+    	Queue<Integer> q = new LinkedList<Integer>();
+    	q.offer(start);
+    	//Queue가 빌 때까지 반복. 방문 정점은 확인, 출력 후 Queue에 넣어 순서대로 확인
+    	while(!q.isEmpty()) {
+    		int out = q.poll();
+    		visits[out] = true;
+    		System.out.print(out+" ");
+    		for (int i = 1; i <= N; i++) {
+				if(map[i][out] == 1 && !visits[i] && i!=out) {
+					q.offer(i);
+					visits[i]=true;
+				}
+			}
+    	}
     }
 }
