30ee7b1628402aa2d0b28cbac2d0f06687e588c7
parkjoohan <joohanpark1513@gmail.com>
2021-09-20 02:46:12 +0900

    UPDATE(21.09.20)

---
+++ src/BaekJoon/P_2446_별찍기5.java
@@ -0,0 +1,21 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class P_2446_별찍기5 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		
+		for (int i = 1; i <= N; i++) {
+			for (int j = 0; j < N-i; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 0; j < i*2-1; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
