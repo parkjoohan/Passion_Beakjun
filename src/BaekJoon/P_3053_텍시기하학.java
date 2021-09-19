18271062f78f182f32d0360bd52d6f0a2b63c402
parkjoohan <joohanpark1513@gmail.com>
2021-09-19 12:57:52 +0900

    UPDATE(21.09.19)

---
+++ src/BaekJoon/P_3053_텍시기하학.java
@@ -0,0 +1,15 @@
+package BaekJoon;
+
+import java.util.*;
+
+public class P_3053_텍시기하학 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		double R =sc.nextDouble();
+
+		System.out.printf("%.6f", R*R*Math.PI);
+		System.out.println();
+		System.out.printf("%.6f", R*R*2);
+	}
+}
