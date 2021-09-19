e30a6830d69f81dbc21564289fc5505a65b50490
parkjoohan <joohanpark1513@gmail.com>
2021-09-19 14:30:37 +0900

    UPDATE(21.09.19)

--- src/BaekJoon/P_1316_그룹단어체커.java
+++ src/BaekJoon/P_1316_그룹단어체커.java
@@ -4,18 +4,24 @@ import java.util.*;
 
 public class P_1316_그룹단어체커 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) { 
 		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		
-		for (int i = 0; i < N; i++) {
+		int N = sc.nextInt(); 
+		int cnt = N; 
+				
+		for (int tc = 0; tc < N; tc++) {
 			String str = sc.next();
-			char[] arr = new char[str.length()];
+			boolean[] arr = new boolean[26];
 			
-			for (int j = 0; j < str.length(); j++) {
-				arr[j] = 
+			for (int i = 1; i < str.length(); i++) {
+				if(str.charAt(i) != str.charAt(i-1)) {
+					if(arr[str.charAt(i)-97] == true) {
+						cnt--; break;
+					}
+					arr[str.charAt(i-1)-97] = true;
+				}
 			}
 		}
+		System.out.println(cnt);
 	}
-
 }
