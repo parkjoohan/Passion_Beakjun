537c1ac1584cc3d87e519dd6320a1a1e930c03cd
parkjoohan <joohanpark1513@gmail.com>
2021-09-19 12:44:43 +0900

    UPDATE(21.09.19)

---
+++ src/BaekJoon/P_11653_소인수분해.java
@@ -0,0 +1,21 @@
+package BaekJoon;
+
+import java.util.*;
+
+public class P_11653_소인수분해 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int i=2;
+		
+		while(N>=i) {
+			if(N%i==0) {
+				System.out.println(i);
+				N/=i;
+			}
+			else
+				i++;
+		}
+	}
+}
