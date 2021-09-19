ac61c553b0adf10c0214b0cb6ec9ffb68d7dba96
parkjoohan <joohanpark1513@gmail.com>
2021-09-19 19:21:32 +0900

    UPDATE(21.09.19)

--- src/BaekJoon/P_10989_수정렬하기3.java
+++ src/BaekJoon/P_10989_수정렬하기3.java
@@ -1,22 +1,26 @@
 package BaekJoon;
 
+import java.io.*;
 import java.util.*;
 
 public class P_10989_수정렬하기3 {
-
-	public static void main(String[] args) extends Ex{
-		Scanner sc = new Scanner(System.in);
-		int T = sc.nextInt();
-		int[] arr = new int[10];
-
-		for(int i=0; i<T; i++) {
-			arr[i] = sc.nextInt();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+        
+		int N = Integer.parseInt(br.readLine());
+		int[] arr = new int[N];
+        
+		for(int i = 0; i < N; i++){
+			arr[i] = Integer.parseInt(br.readLine());
 		}
-		
-		Arrays.sort(arr);	
-		
-		for(int k=0; k<T; k++) {
-			System.out.println(arr[k]);
-		}	
+        
+		Arrays.sort(arr);
+        
+		for(int i = 0; i < N; i++){
+			sb.append(arr[i]).append('\n');
+		}
+ 
+		System.out.println(sb);
 	}
 }
