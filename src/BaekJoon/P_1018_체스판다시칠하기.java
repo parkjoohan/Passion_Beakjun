f36e4e58a66472cc9233c5e664c2b7d0c147c13f
parkjoohan <joohanpark1513@gmail.com>
2021-09-21 23:19:42 +0900

    UPDATE(21.09.21)

---
+++ src/BaekJoon/P_1018_체스판다시칠하기.java
@@ -0,0 +1,20 @@
+package BaekJoon;
+
+import java.util.*;
+
+public class P_1018_체스판다시칠하기 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int M = sc.nextInt();
+		int[][] arr = new int[N][M];
+		
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				
+			}
+		}
+	}
+
+}
