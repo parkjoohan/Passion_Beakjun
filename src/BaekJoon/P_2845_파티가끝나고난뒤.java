3d614574874a5d0bf27d20c524ce17fc818bbf09
parkjoohan <joohanpark1513@gmail.com>
2021-10-01 16:13:39 +0900

    UPDATE(21.10.01)

---
+++ src/BaekJoon/P_2845_파티가끝나고난뒤.java
@@ -0,0 +1,13 @@
+package BaekJoon;
+
+import java.util.*;
+
+public class P_2845_파티가끝나고난뒤 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int L = sc.nextInt();
+		int P
+	}
+
+}
