022f3982ed19dd3f26f9decea8e79f00990f121b
parkjoohan <joohanpark1513@gmail.com>
2021-10-07 23:01:17 +0900

    UPDATE(21.10.07)

--- src/BaekJoon/P_1075_나누기.java
+++ src/BaekJoon/P_1075_나누기.java
@@ -1,28 +1,20 @@
 package BaekJoon;
 
-import java.io.BufferedReader; 
-import java.io.IOException; 
-import java.io.InputStreamReader; 
+import java.util.*; 
 
 public class P_1075_나누기 { 
-	public static void main(String[] args) throws IOException { 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
-		int n = Integer.parseInt(br.readLine()); 
-		int f = Integer.parseInt(br.readLine()); 
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int F = sc.nextInt();
+		N = (N/100) * 100; // 마지막 두자리 00으로 바꾸기
 		
-		// 맨 뒤 2자리를 없애줌 
-		int temp = (n/100)*100; 
-		while(true) { 
-			// 나누어 떨어질때까지 temp의 값을 늘려가면서 비교 
-			if(temp%f == 0) { 
-				int result = temp % 100; 
-				if(result < 10) 
-					System.out.println("0" + result); 
-				else 
-					System.out.println(result); 
-				return; 
-			} 
-			temp++; 
-		} 
-	}​
+		while(N%F != 0) // 나누어 떨어지는 수 찾기
+			N++;
+		
+		N %= 100;
+		
+		if(N < 10) System.out.println("0" + N);
+		else System.out.println(N);
+	}
 }
