81cdc047405b120d0ffd875f74d8cfe4fcb74864
alex0317@naver.com <joohanpark1513@gmail.com>
2021-12-17 00:24:58 +0900

    UPDATE(21.12.17)

---
+++ src/BaekJoon/P_10709_기상캐스터.java
@@ -0,0 +1,30 @@
+package BaekJoon;
+
+import java.util.*;
+
+public class P_10709_기상캐스터 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int H = sc.nextInt();
+		int W = sc.nextInt();
+		int cnt=0;
+		
+		for (int i = 0; i < H; i++) {
+			String str = sc.next();
+			for (int j = 0; j < W; j++) {
+				if(str.charAt(j)=='.' && cnt==0)
+					System.out.print(-1+" ");
+				else if(str.charAt(j)=='.' && cnt!=0) {
+					System.out.print(cnt+" ");
+					cnt++;
+				}
+				else if(str.charAt(j)=='c') {
+					System.out.print(0+" ");
+					cnt=1;
+				}
+			}
+			System.out.println();
+			cnt=0;
+		}
+	}
+}
