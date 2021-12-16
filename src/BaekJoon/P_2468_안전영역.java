faceb5951389073d7e0a5e8bbf23fbc3fc4b460d
alex0317@naver.com <joohanpark1513@gmail.com>
2021-12-16 22:00:32 +0900

    UPDATE(21.12.16)

--- src/BaekJoon/P_2468_안전영역.java
+++ src/BaekJoon/P_2468_안전영역.java
@@ -2,64 +2,124 @@ package BaekJoon;
 
 import java.util.*;
 
+//public class P_2468_안전영역 {
+//
+//	static int N, cnt, max;		// 안전 지역
+//	static int[][] map;
+//	static int[][] change;
+//	static boolean[][] check;
+//	
+//	static int[] dr = {-1,1,0,0};	//상하좌우
+//	static int[] dc = {0,0,-1,1};	
+//	public static void main(String[] args) {
+//		Scanner sc = new Scanner(System.in);
+//		N = sc.nextInt();
+//		map = new int[N][N];
+//		max = 1;
+//		
+//		for(int i=0; i<N; i++) {
+//			for(int j=0; j<N; j++)
+//				map[i][j] = sc.nextInt();
+//		}
+//		
+//		for(int i=1; i<=100; i++) {
+//			change = new int[N][N];
+//			check = new boolean[N][N];
+//			cnt=0;
+//			
+//			// 높이가 해당 높이 보다 낮으면 0 높으면 1
+//			for(int j=0; j<N; j++) {
+//				for(int k=0; k<N; k++) {
+//					if(map[j][k] <= i)
+//						change[j][k]=0;
+//					else
+//						change[j][k]=1;
+//				}
+//			}
+//			for(int j=0; j<N; j++) {
+//				for(int k=0; k<N; k++) {
+//					if(change[j][k]==1 && !check[j][k]) {
+//						func(j,k);
+//						cnt++;
+//					}
+//				}
+//			}
+//			max = Math.max(max,  cnt);
+//		}
+//		System.out.println(max);
+//	}
+//
+//	static void func(int r, int c) {
+//		check[r][c] = true;
+//		
+//		for(int i=0; i<4; i++) {
+//			int nr = r+dr[i];
+//			int nc = c+dc[i];
+//			
+//			if(nr>=0 && nc>=0 && nr<N && nc<N){
+//				if(change[nr][nc]==1 && !check[nr][nc])
+//					func(nr,nc);
+//			}
+//		}
+//	}
+//}
+
 public class P_2468_안전영역 {
 
-	static int N, cnt, max;		// 안전 지역
-	static int[][] map;
-	static int[][] change;
-	static boolean[][] check;
-	
-	static int[] dr = {-1,1,0,0};	//상하좌우
-	static int[] dc = {0,0,-1,1};	
+	static int N, cnt, max;
+	static int map[][];
+	static int change[][];
+	static boolean visited[][];
+	static int dr[] = {-1,1,0,0};
+	static int dc[] = {0,0,-1,1};
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		N = sc.nextInt();
 		map = new int[N][N];
-		max = 1;
+		max=1;
 		
-		for(int i=0; i<N; i++) {
-			for(int j=0; j<N; j++)
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
 				map[i][j] = sc.nextInt();
+			}
 		}
 		
-		for(int i=1; i<=100; i++) {
+		for (int i = 1; i <=100; i++) {
 			change = new int[N][N];
-			check = new boolean[N][N];
+			visited = new boolean[N][N];
 			cnt=0;
 			
-			// 높이가 해당 높이 보다 낮으면 0 높으면 1
-			for(int j=0; j<N; j++) {
-				for(int k=0; k<N; k++) {
-					if(map[j][k] <= i)
+			for (int j = 0; j < N; j++) {
+				for (int k = 0; k < N; k++) {
+					if(map[j][k]<=i)
 						change[j][k]=0;
 					else
 						change[j][k]=1;
 				}
 			}
-			for(int j=0; j<N; j++) {
-				for(int k=0; k<N; k++) {
-					if(change[j][k]==1 && !check[j][k]) {
-						func(j,k);
+			for (int j = 0; j < N; j++) {
+				for (int k = 0; k < N; k++) {
+					if(change[j][k]==1 && !visited[j][k]) {
+						dfs(j,k);
 						cnt++;
 					}
 				}
 			}
-			max = Math.max(max,  cnt);
+			max = Math.max(max, cnt);
 		}
 		System.out.println(max);
 	}
-
-	static void func(int r, int c) {
-		check[r][c] = true;
+	
+	public static void dfs(int r, int c) {
+		visited[r][c]=true;
 		
-		for(int i=0; i<4; i++) {
-			int nr = r+dr[i];
-			int nc = c+dc[i];
+		for (int d = 0; d < 4; d++) {
+			int R = r + dr[d];
+			int C = c + dc[d];
 			
-			if(nr>=0 && nc>=0 && nr<N && nc<N){
-				if(change[nr][nc]==1 && !check[nr][nc])
-					func(nr,nc);
-			}
+			if(R>=0 && C>=0 && R<N && C<N)
+				if(change[R][C]==1 && !visited[R][C])
+					dfs(R,C);
 		}
 	}
 }
