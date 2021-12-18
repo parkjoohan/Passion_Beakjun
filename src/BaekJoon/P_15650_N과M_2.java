e65d14291a277370feea105f3f2428b5a8cb5b17
alex0317@naver.com <joohanpark1513@gmail.com>
2021-12-19 00:16:28 +0900

    UPDATE(21.12.19)

--- src/BaekJoon/P_15650_N과M_2.java
+++ src/BaekJoon/P_15650_N과M_2.java
@@ -3,44 +3,83 @@ package BaekJoon;
 // 조합
 import java.util.Scanner;
 
+//public class P_15650_N과M_2 {
+//	static int N, M;
+//	public static int[] arr;
+//	public static boolean[] check;
+// 
+//	public static void main(String[] args) {
+// 
+//		Scanner in = new Scanner(System.in);
+// 
+//		N = in.nextInt();
+//		M = in.nextInt();
+// 
+//		arr = new int[M];
+//		check = new boolean[N];
+//		Result(0);
+// 
+//	}
+// 
+//	public static void Result(int idx) {
+//		if (idx == M) {
+//			for(int i=0; i<M-1; i++) {
+//				if(arr[i] > arr[i+1])
+//					return;
+//			}
+//			
+//			for (int num : arr) {
+//				System.out.print(num + " ");
+//			}
+//			System.out.println();
+//			return;
+//		}
+// 
+//		for (int i = 0; i < N; i++) {
+//			if (!check[i]) {
+//				check[i] = true;
+//				arr[idx] = i + 1;
+//				Result(idx + 1);
+//				check[i] = false;
+//			}
+//		}
+//	}
+//}
+
 public class P_15650_N과M_2 {
+
 	static int N, M;
-	public static int[] arr;
-	public static boolean[] check;
- 
+	static int[] arr;
+	static boolean[] visited;
 	public static void main(String[] args) {
- 
-		Scanner in = new Scanner(System.in);
- 
-		N = in.nextInt();
-		M = in.nextInt();
- 
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		M = sc.nextInt();
 		arr = new int[M];
-		check = new boolean[N];
-		Result(0);
- 
+		visited = new boolean[N];
+		
+		comb(N, M, 0);
 	}
- 
-	public static void Result(int idx) {
-		if (idx == M) {
+	
+	public static void comb(int N, int M, int idx) {
+		if(idx==M) {
 			for(int i=0; i<M-1; i++) {
 				if(arr[i] > arr[i+1])
 					return;
 			}
-			
-			for (int num : arr) {
-				System.out.print(num + " ");
+			for(int num : arr) {
+				System.out.print(num+" ");
 			}
 			System.out.println();
 			return;
 		}
- 
+		
 		for (int i = 0; i < N; i++) {
-			if (!check[i]) {
-				check[i] = true;
-				arr[idx] = i + 1;
-				Result(idx + 1);
-				check[i] = false;
+			if(!visited[i]) {
+				visited[i] = true;
+				arr[idx] = i+1;
+				comb(N, M, idx+1);
+				visited[i] = false;
 			}
 		}
 	}
