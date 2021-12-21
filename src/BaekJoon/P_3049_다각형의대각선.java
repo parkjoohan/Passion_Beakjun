2fc4854c65f5bf79df8c78cadb8999eb820c0352
alex0317@naver.com <joohanpark1513@gmail.com>
2021-12-21 21:36:00 +0900

    UPDATE(21.12.21)

---
+++ src/BaekJoon/P_3049_다각형의대각선.java
@@ -0,0 +1,12 @@
+package BaekJoon;
+
+import java.util.*;
+
+public class P_3049_다각형의대각선 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		System.out.println((N*(N-1)*(N-2)*(N-3)) / 24);
+	}
+}
