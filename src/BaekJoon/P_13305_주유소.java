28e7c0686ccc6d58ee3b9d3a0c66e0e7e087fa6a
alex0317@naver.com <joohanpark1513@gmail.com>
2022-01-14 22:46:13 +0900

    UPDATE(22.01.14)

--- src/BaekJoon/P_13305_주유소.java
+++ src/BaekJoon/P_13305_주유소.java
@@ -1,10 +1,31 @@
 package BaekJoon;
 
+import java.util.*;
+
 public class P_13305_주유소 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int[] road = new int[N-1];
+		int[] oil = new int[N];
+		
+		for (int i = 0; i < N-1; i++) {
+			road[i] = sc.nextInt();
+		}
+		
+		for (int i = 0; i < N; i++) {
+			oil[i] = sc.nextInt();
+		}
+		long sum = 0;
+		long min = oil[0];
+		
+		for (int i = 0; i < N-1; i++) {
+			if(oil[i]<min)
+				min=oil[i];
+			
+			sum += (min * road[i]);
+		}
+		System.out.println(sum);
 	}
-
 }
