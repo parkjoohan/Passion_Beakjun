1b2574c5d1e8c76b65fb9778ea11b41d25d80df3
alex0317@naver.com <joohanpark1513@gmail.com>
2022-01-26 22:23:37 +0900

    UPDATE(22.01.26)

--- src/BaekJoon/P_1015_수열정렬.java
+++ src/BaekJoon/P_1015_수열정렬.java
@@ -5,7 +5,13 @@ import java.util.*;
 public class P_1015_수열정렬 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int[] arr = new int[N];
+		
+		for (int i = 0; i < N; i++) {
+			
+		}
 
 	}
 
