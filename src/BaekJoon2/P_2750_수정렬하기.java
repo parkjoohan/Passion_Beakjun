0830d9547d78d5d15ad3c8597e4cf18538412a66
alex0317@naver.com <joohanpark1513@gmail.com>
2022-03-04 08:22:22 +0900

    UPDATE(22.03.04)

---
+++ src/BaekJoon2/P_2750_수정렬하기.java
@@ -0,0 +1,22 @@
+package BaekJoon2;
+
+import java.util.*;
+
+public class P_2750_수정렬하기 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int[] arr = new int[N];
+		
+		for (int i = 0; i < N; i++) {
+			arr[i] = sc.nextInt();
+		}
+
+		Arrays.sort(arr);
+		
+		for (int i = 0; i < N; i++) {
+			System.out.println(arr[i]);
+		}
+	}
+}
