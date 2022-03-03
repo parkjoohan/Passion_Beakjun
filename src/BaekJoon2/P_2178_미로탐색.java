da0ace63cb86f3ec81eca1b340277a6640097db8
alex0317@naver.com <joohanpark1513@gmail.com>
2022-03-04 00:25:15 +0900

    UPDATE(22.03.04)

--- src/BaekJoon2/P_2178_미로탐색.java
+++ src/BaekJoon2/P_2178_미로탐색.java
@@ -3,57 +3,63 @@ package BaekJoon2;
 import java.util.*;
 
 public class P_2178_미로탐색 {
-    static int[] dx = {0,0,1,-1};
-    static int[] dy = {1,-1,0,0};
-    static int[][] maze;
-    static boolean[][] check;
-    static int n,m;
-
+    
+	static int N, M;
+	static int[][] map;
+	static boolean[][] visited;
+	static int[] dx = {0,1,0,-1};
+	static int[] dy = {1,0,-1,0};
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        m = sc.nextInt();
-        maze = new int[n][m];
-        check = new boolean[n][m];
-
-        for(int i = 0;i<n;i++){
-            String tmp = sc.next();
-            for(int j = 0;j<m;j++){
-                maze[i][j] = tmp.charAt(j) - '0';
-            }
-        }
-        bfs();
-        System.out.println(maze[n-1][m-1]);
+    	Scanner sc = new Scanner(System.in);
+    	N = sc.nextInt();
+    	M = sc.nextInt();
+    	map = new int[N][M];
+    	visited = new boolean[N][M];
+    	
+    	for (int i = 0; i < N; i++) {
+    		String str = sc.next();
+			for (int j = 0; j < M; j++) {
+				map[i][j] = str.charAt(j) - '0';
+			}
+		}
+    	
+    	bfs();
+    	System.out.println(map[N-1][M-1]);
     }
-
-    private static void bfs() {
-        Queue<Integer> queue_x = new LinkedList<Integer>();
-        Queue<Integer> queue_y = new LinkedList<Integer>();
-
-        queue_x.offer(0);
-        queue_y.offer(0);
-
-        check[0][0] = true;
-
-        while(!queue_x.isEmpty()){
-            int x = queue_x.poll();
-            int y = queue_y.poll();
-
-            for(int i = 0; i<4;i++){
-                int nx = x + dx[i];
-                int ny = y + dy[i];
-
-                if(nx >=0 && ny >=0 && nx<n && ny < m){
-                    if(maze[nx][ny] == 1 && !check[nx][ny]){
-                        queue_x.offer(nx);
-                        queue_y.offer(ny);
-
-                        check[nx][ny] = true;
-
-                        maze[nx][ny] = maze[x][y] + 1;
-                    }
-                }
-            }
-        }
+    
+    public static void bfs() {
+    	
+    	// x, y에 대한 Queue
+    	Queue<Integer> q_x = new LinkedList<Integer>();
+    	Queue<Integer> q_y = new LinkedList<Integer>();
+    	
+    	// 저장
+    	q_x.offer(0);
+    	q_y.offer(0);
+    	
+    	visited[0][0] = true;
+    	
+    	while(!q_x.isEmpty()) {
+    		int x = q_x.poll();
+    		int y = q_y.poll();
+    		
+    		// 상하좌우 확인
+    		for (int k = 0; k < 4; k++) {
+				int temp_x = x + dx[k];
+				int temp_y = y + dy[k];
+				
+				// 유효한 범위인지 확인
+				if(temp_x >= 0 && temp_y >= 0 && temp_x < N && temp_y < M) {
+					if(map[temp_x][temp_y]==1 && visited[temp_x][temp_y]==false) {
+						q_x.offer(temp_x);
+						q_y.offer(temp_y);
+						
+						visited[temp_x][temp_y] = true;
+						
+						map[temp_x][temp_y] = map[x][y] + 1;
+					}
+				}
+			}
+    	}
     }
 }
