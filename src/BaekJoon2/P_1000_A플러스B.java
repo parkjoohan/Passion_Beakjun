6b4a35260996b6398186e636575e9b89be8ec090
alex0317@naver.com <joohanpark1513@gmail.com>
2022-03-12 20:23:50 +0900

    UPDATE(22.03.12)

---
+++ src/BaekJoon2/P_1000_A플러스B.java
@@ -0,0 +1,17 @@
+package BaekJoon2;
+
+import java.util.Scanner;
+
+public class P_1000_A플러스B {
+ 
+	public static void main(String[] args) {
+ 
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		
+		System.out.println(A+B);
+ 
+		sc.close();
+	}
+}
