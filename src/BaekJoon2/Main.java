5c2bad74ddc77edd15a7d5d5baad48f4392f0f21
alex0317@naver.com <joohanpark1513@gmail.com>
2022-06-24 22:18:09 +0900

    UPDATE(22.06.24)

--- src/BaekJoon2/Main.java
+++ src/BaekJoon2/Main.java
@@ -4,30 +4,38 @@ import java.util.*;
 
 public class Main {
 
-	static int N, cnt;
+	static int N, M, K, cnt=0;
 	static int[][] map;
-	static boolean[][] visited;
 	static int[] dx = {-1,1,0,0};
 	static int[] dy = {0,0,-1,1};
-	static ArrayList<Integer> list = new ArrayList<>();
+	static ArrayList<Integer> list;
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		M = sc.nextInt();
 		N = sc.nextInt();
-		map = new int[N][N];
-		visited = new boolean[N][N];
-		cnt = 0;
+		K = sc.nextInt();
+		map = new int[M][N];
 		
-		for(int i=0; i<N; i++) {
-			String str = sc.next();
-			for(int j=0; j<N; j++) {
-				map[i][j]=str.charAt(j)-'0';
+		for(int i=0; i<K; i++) {
+			int x1 = sc.nextInt();
+			int y1 = sc.nextInt();
+			int x2 = sc.nextInt();
+			int y2 = sc.nextInt();
+			
+			for(int j=y1; j<y2; j++) {
+				for(int k=x1; k<x2; k++) {
+					map[j][k]=1;
+				}
 			}
 		}
 		
-		for(int i=0; i<N; i++) {
+		// 영역 넓이 저장
+		list = new ArrayList<>();
+		
+		for(int i=0; i<M; i++) {
 			for(int j=0; j<N; j++) {
-				if(map[i][j]==1 && !visited[i][j]) {
-					cnt=1;
+				if(map[i][j]==0) {
+					cnt=0;
 					dfs(i, j);
 					list.add(cnt);
 				}
@@ -37,24 +45,21 @@ public class Main {
 		Collections.sort(list);
 		System.out.println(list.size());
 		for(int num : list)
-			System.out.println(num);
+			System.out.print(num+" ");
 	}
 	
-	public static int dfs(int x, int y) {
-		visited[x][y]=true;
+	public static void dfs(int x, int y) {
+		map[x][y]=1;
+		cnt++;
 		
 		for(int i=0; i<4; i++) {
 			int nx = x + dx[i];
 			int ny = y + dy[i];
 			
-			if(nx>=0 && ny>=0 && nx<N && ny<N) {
-				if(map[nx][ny]==1 && !visited[nx][ny]) {
-					dfs(nx, ny);
-					cnt++;
+			if(nx>=0 && ny>=0 && nx<M && ny<N)
+				if(map[nx][ny]==0) {
+					dfs(nx,ny);
 				}
-			}
 		}
-		
-		return cnt;
 	}
 }
--- src/BaekJoon2/P_2583_영역구하기.java
+++ src/BaekJoon2/P_2583_영역구하기.java
@@ -4,9 +4,9 @@ import java.util.*;
 
 public class P_2583_영역구하기 {
 
-	static int N,M,K, cnt=0;
+	static int N, M, K, cnt=0;
 	static int[][] map;
-	static int[] dx = {1,-1,0,0};
+	static int[] dx = {-1,1,0,0};
 	static int[] dy = {0,0,-1,1};
 	static ArrayList<Integer> list;
 	public static void main(String[] args) {
@@ -16,49 +16,50 @@ public class P_2583_영역구하기 {
 		K = sc.nextInt();
 		map = new int[M][N];
 		
-		for (int i = 0; i < K; i++) {
+		for(int i=0; i<K; i++) {
 			int x1 = sc.nextInt();
 			int y1 = sc.nextInt();
 			int x2 = sc.nextInt();
 			int y2 = sc.nextInt();
 			
-			for (int j = y1; j < y2; j++) {
-				for (int k = x1; k < x2; k++) {
+			for(int j=y1; j<y2; j++) {
+				for(int k=x1; k<x2; k++) {
 					map[j][k]=1;
 				}
 			}
 		}
 		
-		// 영역 넒이 저장
+		// 영역 넓이 저장
 		list = new ArrayList<>();
 		
-		for (int i = 0; i < M; i++) {
-			for (int j = 0; j < N; j++) {
+		for(int i=0; i<M; i++) {
+			for(int j=0; j<N; j++) {
 				if(map[i][j]==0) {
-					cnt = 0;
-					dfs(i,j);
+					cnt=0;
+					dfs(i, j);
 					list.add(cnt);
 				}
 			}
 		}
 		
-		System.out.println(list.size());
 		Collections.sort(list);
-		
-		for(Integer c : list)	System.out.print(c+" ");
+		System.out.println(list.size());
+		for(int num : list)
+			System.out.print(num+" ");
 	}
 	
-	public static void dfs(int r, int c) {
-		map[r][c] = 1;
+	public static void dfs(int x, int y) {
+		map[x][y]=1;
 		cnt++;
 		
-		for (int i = 0; i < 4; i++) {
-			int nr = r + dx[i];
-			int nc = c + dy[i];
+		for(int i=0; i<4; i++) {
+			int nx = x + dx[i];
+			int ny = y + dy[i];
 			
-			if(nr>=0 && nc>=0 && nr<M && nc<N)
-				if(map[nr][nc]==0)
-					dfs(nr, nc);
+			if(nx>=0 && ny>=0 && nx<M && ny<N)
+				if(map[nx][ny]==0) {
+					dfs(nx,ny);
+				}
 		}
 	}
 }
