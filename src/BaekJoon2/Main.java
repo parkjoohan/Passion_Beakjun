5b457c97e5a2b9ad1a67835c10383f435ba3ec70
alex0317@naver.com <joohanpark1513@gmail.com>
2022-06-24 23:34:59 +0900

    UPDATE(22.06.24)

--- src/BaekJoon2/Main.java
+++ src/BaekJoon2/Main.java
@@ -4,70 +4,49 @@ import java.util.*;
 
 public class Main {
 
-	static int N;
+	static int T, M, N, K;
 	static int[][] map;
 	static boolean[][] visited;
 	static int[] dx = {-1,1,0,0};
 	static int[] dy = {0,0,-1,1};
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		N = sc.nextInt();
-		map = new int[N][N];
-		// 배열 정수 중 높이 최댓값
-		int max = 0;
-		// 안전영역 최대인 갯수
-		int result = 1;
+		T = sc.nextInt();
 		
-		for(int i=0; i<N; i++) {
-			for(int j=0; j<N; j++) {
-				map[i][j]=sc.nextInt();
-				if(map[i][j]>max)
-					max = map[i][j];
-			}
-		}
-		
-		// 1부터 최대 높이까지 안전영역이 최대인 갯수 찾기
-		for(int i=1; i<=max; i++) {
-			// 각 높이마다 방문 영역 초기화
-			visited = new boolean[N][N];
-			// 각 높이마다 안전영역의 갯수 초기화
+		for(int tc=0; tc<T; tc++) {
+			M = sc.nextInt();
+			N = sc.nextInt();
+			K = sc.nextInt();
+			map = new int[M][N];
+			visited = new boolean[M][N];
 			int cnt=0;
 			
-			for(int j=0; j<N; j++) {
-				for(int k=0; k<N; k++) {
-					// 잠기지 않고 방문하지 않은 곳
-					if(map[j][k]>i && !visited[j][k]) {
+			for(int i=0; i<K; i++)
+				map[sc.nextInt()][sc.nextInt()] = 1;
+			
+			for(int i=0; i<M; i++) {
+				for(int j=0; j<N; j++) {
+					if(map[i][j]==1 && !visited[i][j]) {
+						dfs(i,j);
 						cnt++;
-						dfs(j,k,i);
 					}
 				}
 			}
-			result = Math.max(result, cnt);
+			
+			System.out.println(cnt);
 		}
-		System.out.println(result);
 	}
 	
-	public static void dfs(int x, int y, int h) {
-		
-		// x, y 좌표가 지도의 범위를 벗어나는 경우
-		if(x<0 || y<0 || x>=N || y>=N)
-			return;
-		
-		// 이미 방문한 위치거나 최대 높이 이하로 잠긴 경우
-		if(visited[x][y] || map[x][y]<=h)
-			return;
-		
+	public static void dfs(int x, int y) {
 		visited[x][y]=true;
 		
 		for(int i=0; i<4; i++) {
 			int nx = x + dx[i];
 			int ny = y + dy[i];
 			
-			// 해당 지도 내여야함
-			if(nx>=0 && ny>=0 && nx<N && ny<N) {
-				// 잠기지 않고 방문하지 않은 곳
-				if(!visited[nx][ny] && map[nx][ny]>h)
-					dfs(nx,ny,h);
+			if(nx>=0 && ny>=0 && nx<M && ny<N) {
+				if(map[nx][ny]==1 && !visited[nx][ny])
+					dfs(nx,ny);
 			}
 		}
 	}
--- src/BaekJoon2/P_1012_유기농배추.java
+++ src/BaekJoon2/P_1012_유기농배추.java
@@ -2,9 +2,9 @@ package BaekJoon2;
 
 import java.util.*;
 
-public class P_1012_유기농배추 {
-	
-	static int T, N, M, K;
+public class Main {
+
+	static int T, M, N, K;
 	static int[][] map;
 	static boolean[][] visited;
 	static int[] dx = {-1,1,0,0};
@@ -13,7 +13,7 @@ public class P_1012_유기농배추 {
 		Scanner sc = new Scanner(System.in);
 		T = sc.nextInt();
 		
-		for (int tc = 0; tc < T; tc++) {
+		for(int tc=0; tc<T; tc++) {
 			M = sc.nextInt();
 			N = sc.nextInt();
 			K = sc.nextInt();
@@ -21,32 +21,32 @@ public class P_1012_유기농배추 {
 			visited = new boolean[M][N];
 			int cnt=0;
 			
-			for (int i = 0; i < K; i++) {
+			for(int i=0; i<K; i++)
 				map[sc.nextInt()][sc.nextInt()] = 1;
-			}
 			
-			for (int i = 0; i < M; i++) {
-				for (int j = 0; j < N; j++) {
+			for(int i=0; i<M; i++) {
+				for(int j=0; j<N; j++) {
 					if(map[i][j]==1 && !visited[i][j]) {
 						dfs(i,j);
 						cnt++;
 					}
 				}
 			}
+			
 			System.out.println(cnt);
 		}
 	}
 	
-	public static void dfs(int r, int c) {
-		visited[r][c] = true;
+	public static void dfs(int x, int y) {
+		visited[x][y]=true;
 		
-		for (int i = 0; i < 4; i++) {
-			int nr = r + dx[i];
-			int nc = c + dy[i];
+		for(int i=0; i<4; i++) {
+			int nx = x + dx[i];
+			int ny = y + dy[i];
 			
-			if(nr>=0 && nc>=0 && nr<M && nc<N) {
-				if(map[nr][nc]==1 && !visited[nr][nc])
-					dfs(nr, nc);
+			if(nx>=0 && ny>=0 && nx<M && ny<N) {
+				if(map[nx][ny]==1 && !visited[nx][ny])
+					dfs(nx,ny);
 			}
 		}
 	}
