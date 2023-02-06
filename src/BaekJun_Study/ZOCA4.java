6fbc31c708f82aa73731958c3b27af084324db3a
alex0317@naver.com <joohanpark1513@gmail.com>
2023-02-07 01:37:25 +0900

    코테 준비

--- src/BaekJun_Study/ZOCA4.java
+++ src/BaekJun_Study/ZOCA4.java
@@ -1,10 +1,21 @@
 package BaekJun_Study;
 
+import java.util.*;
+
 public class ZOCA4 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		int H = sc.nextInt();
+		int W = sc.nextInt();
+		int N = sc.nextInt();
+		int M = sc.nextInt();
+		
+		int height = H%(N+1) == 0 ? H/(N+1) : H/(N+1)+1;
+		int width = W%(M+1) == 0 ? W/(M+1) : W/(M+1)+1;
+		
+		System.out.println(height * width);
 	}
 
 }
