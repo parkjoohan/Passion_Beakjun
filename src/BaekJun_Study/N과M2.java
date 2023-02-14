17cc4b88d13c6efe3f9917481858ff7644c99dc9
alex0317@naver.com <joohanpark1513@gmail.com>
2023-02-14 23:44:27 +0900

    코테 준비

--- src/BaekJun_Study/N과M2.java
+++ src/BaekJun_Study/N과M2.java
@@ -6,35 +6,26 @@ public class N과M2 {
 	
 	static int N, M;
 	static int[] arr;
-	static boolean[] visited;
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 		N = sc.nextInt();
 		M = sc.nextInt();
-		arr = new int[N+1];
-		visited = new boolean[N+1];
+		arr = new int[M];
 		
-		back(0);
+		dfs(1, 0);
 	}
-
-	private static void back(int index) {
-		if(index == M) {
-			for(int i=0; i<M; i++)
-				System.out.print(arr[i] + " ");
+	
+	public static void dfs(int at, int depth) {
+		if(depth == M) {
+			for(int val : arr)
+				System.out.print(val + " ");
 			System.out.println();
+			return;
 		}
 		
-		for(int i=1; i<=N; i++) {
-			if(!visited[i]) {
-                arr[index] = i;
-                visited[i] = true;
-                back(index + 1);
-                // 재귀 호출이 끝난 후에 현재 i만 true로 해두고 나머지는 false로 바꾼다.
-                for (int j = i + 1; j <= N; j++) {
-                    visited[j] = false;
-                }
-            }
+		for(int i=at; i<=N; i++) {
+			arr[depth] = i;
+			dfs(i+1, depth+1);
 		}
 	}
 }
