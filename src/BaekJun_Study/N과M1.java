9f9316de4b4c708e144864b5a46fcd8bc4d12365
alex0317@naver.com <joohanpark1513@gmail.com>
2023-02-14 22:02:24 +0900

    코테 준비

--- src/BaekJun_Study/N과M1.java
+++ src/BaekJun_Study/N과M1.java
@@ -17,6 +17,7 @@ public class N과M1 {
 	}
 	
 	public static void dfs(int N, int M, int depth) {
+		// 재귀 깊이가 M과 같아지면 탐색과정에서 담았던 배열을 출력
 		if(depth == M) {
 			for(int val : arr)
 				System.out.print(val + " ");
@@ -25,11 +26,12 @@ public class N과M1 {
 		}
 		
 		for(int i=0; i<N; i++) {
+			// 만약 해당 노드(값)을 방문하지 않았다면?
 			if(!visited[i]) {
-				visited[i] = true;
-				arr[depth] = i+1;
-				dfs(N, M, depth+1);
-				visited[i] = false;
+				visited[i] = true;		// 해당 노드를 방문상태로 변경
+				arr[depth] = i+1;		// 해당 깊이를 index로 하여 i+1 값 저장
+				dfs(N, M, depth+1);		// 다음 자식 노드 방문을 위해 depth을 1 증가시키면서 재귀호출
+				visited[i] = false;		// 자식노드 방문이 끝나고 돌아오면 방문노드를 방문하지 않은 상태로 변경
 			}
 		}
 	}
